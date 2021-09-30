e606b69d734e31c98967b9fc7df0d6da9f075fa4
damien2709 <damien2709@hotmail.fr>
2021-09-30 15:13:00 +0200

    PUT-DELETE-GET-GET pour les sauces OK

--- controllers/controllerSauce.js
+++ controllers/controllerSauce.js
@@ -6,7 +6,7 @@ const Sauce = require('../models/modelSauce');
 // on va importer le package "fs" de nodeJS, qui nous donne accès aux fonctions qui nous permettent de modifier le système de fichiers, y compris aux fonctions permettant de supprimer les fichiers.
 const fs = require('fs');
 
-//LOGIQUE MéTIER POUR CRéER UN ARTICLE : pour chaque POST envoyé (donc une sauce), on va créer un "modèle" sauce (thing) que l'on va enregistrer dans la BDD. Tout ça sera encapsulé dans une fonction "createThing" qu'on exportera pour l'utiliser ailleurs. Le nom de la fonction doit expliquer clairement son rôle !
+//POST : LOGIQUE MéTIER POUR CRéER UN ARTICLE : pour chaque POST envoyé (donc une sauce), on va créer un "modèle" sauce (thing) que l'on va enregistrer dans la BDD. Tout ça sera encapsulé dans une fonction "createThing" qu'on exportera pour l'utiliser ailleurs. Le nom de la fonction doit expliquer clairement son rôle !
 exports.createSauce = (req, res, next) => {
   //•	Pour ajouter un fichier à la requête, le front-end doit envoyer les données de la requête sous la forme form-data, et non sous forme de JSON. Le corps de la requête contient une chaîne sauce , qui est simplement un objet Sauce converti en chaîne. Nous devons donc l'analyser à l'aide de JSON.parse() pour obtenir un objet utilisable.
   const sauceObject = JSON.parse(req.body.sauce);    
@@ -30,11 +30,11 @@ exports.createSauce = (req, res, next) => {
       sauce.save()
         // on doit renvoyer une réponse à l'appli frontend sinon on aura l'expiration de la requête !
         .then(
-          () => res.json({message : 'sauce enregistrée'})) // on envoie un statut et un message en json. 
+          () => res.json({message : 'sauce enregistrée'})) // on envoie un statut (ca fait buguer !!!) et un message en json. 
         .catch(error => res.status(400).json({ error})); // on récupère l'erreur et on renvoie un code 400 puis un message d'erreur dans un objet. "error" est un raccourci JS qui veut dire : (error : error) 
 };
 
-//LOGIQUE MéTIER POUR MODIFIER UNE SAUCE : La fonction "updateSauce" pour une requête de type PUT permettant de modifier, mettre à jour un article identifié par son ID dans la BDD. Nous devons d'abord savoir si le user a modifié le fichier image ou pas (car si oui on aura un objet form-data, si non on aura un objet json). On cherche donc à savoir s'il y a un fichier dans la requête. Ensuite, nous utilisons la méthode updateOne() dans notre modèle/classe "Sauce"  pour modifier la sauce unique ayant le même _id que le paramètre de la requête ; La méthode updateOne() renvoie une promesse. Elle prend 2 arguments : le 1er est l'objet de comparaison donc on récupère l'id des paramètres de route, le 2ème argument est le nouvel objet ou sa nouvelle version (on utilise le spread operator).
+//PUT : LOGIQUE MéTIER POUR MODIFIER UNE SAUCE : La fonction "updateSauce" pour une requête de type PUT permettant de modifier, mettre à jour un article identifié par son ID dans la BDD. Nous devons d'abord savoir si le user a modifié le fichier image ou pas (car si oui on aura un objet form-data, si non on aura un objet json). On cherche donc à savoir s'il y a un fichier dans la requête. Ensuite, nous utilisons la méthode updateOne() dans notre modèle/classe "Sauce"  pour modifier la sauce unique ayant le même _id que le paramètre de la requête ; La méthode updateOne() renvoie une promesse. Elle prend 2 arguments : le 1er est l'objet de comparaison donc on récupère l'id des paramètres de route, le 2ème argument est le nouvel objet ou sa nouvelle version (on utilise le spread operator).
 exports.modifySauce = (req, res, next) => {
   const sauceObject = req.file ? //ici on utilise l'opérateur ternaire pour savoir si req.file existe (donc s'il y a un fichier dans la requête). S'il existe on aura un type d'objet (form-data) et si non un autre type d'objet (json). On résout l'équation de la sorte "{form-data} : {json}", ce qui veut dire si oui 1er objet, si non, 2ème objet. 
     {
@@ -43,13 +43,13 @@ exports.modifySauce = (req, res, next) => {
     } : { ...req.body };
 
   Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Sauce modifiée !'}))
+        .then(() => res.json({ message: 'Sauce modifiée !'})) //si j'ajoute le code de statut Ca ne fonctionne pas !
         .catch(error => res.status(400).json({ error }))
 };
 
-//LOGIQUE MéTIER POUR SUPPRIMER UNE SAUCE : la fonction "deleteSauce" pour une requête de type DELETE permettant de supprimer une sauce identifiée par son ID dans la BDD.Nous utilisons la méthode deleteOne() dans notre modèle/classe "Sauce" (QUI EST UN FICHIER "Sauce" DANS LA BDD !)pour modifier la sauce unique ayant le même _id que le paramètre de la requête ; La méthode updateOne() renvoie une promesse. Elle prend 1 argument : l'objet de comparaison.
+//DELETE : LOGIQUE MéTIER POUR SUPPRIMER UNE SAUCE : la fonction "deleteSauce" pour une requête de type DELETE permettant de supprimer une sauce identifiée par son ID dans la BDD.Nous utilisons la méthode deleteOne() dans notre modèle/classe "Sauce" (QUI EST UN FICHIER "Sauce" DANS LA BDD !)pour modifier la sauce unique ayant le même _id que le paramètre de la requête ; La méthode updateOne() renvoie une promesse. Elle prend 1 argument : l'objet de comparaison.
 exports.deleteSauce = (req, res, next) => {
-  Sauce.findOne({ _id: req.params.id }) //on va chercher le fichier dont l'ID correspond à celle de la requête.
+  Sauce.findOne({ _id: req.params.id }) //on va chercher le fichier de la BDD dont l'ID correspond à celle de la requête.
     .then(sauce => {
       const filename = sauce.imageUrl.split('/images/')[1];// on récupère l'url du fichier de l'objet qu'on splite après la partie /images/ pour récupérer le nom du fichier. Le split retourne un tableau de 2 éléments, on garde le 2ème élément qui a l'index 1 et qui est le nom du fichier.
       //ensuite on utilise la méthode unlink de fs (pour supprimer un fichier attaché à un objet). elle prend en 1er argument la chaine de caractères correspondant au chemin du fichier, en 2ème argument une fonction de callback qui sera ici la suppression de l'objet Thing de la BDD. 
@@ -62,14 +62,14 @@ exports.deleteSauce = (req, res, next) => {
     .catch(error => res.status(500).json({ error })); // erreur serveur
 };
 
-//LOGIQUE MéTIER POUR LIRE UNE SAUCE UNIQUE : je veux renvoyer une sauce unique identifiée par son ID (créé par la BDD lors de la requête POST). Nous utilisons la méthode findOne() dans notre modèle/classe "Sauce" (QUI EST UN FICHIER "Sauce" DANS LA BDD !) pour trouver la sauce unique ayant le même _id que le paramètre de la requête ; La méthode findOne() renvoie une promesse. Elle prend comme argument l'objet de comparaison donc on récupère l'ID des paramètres de route.
+//GET : LOGIQUE MéTIER POUR LIRE UNE SAUCE UNIQUE : je veux renvoyer une sauce unique identifiée par son ID (créé par la BDD lors de la requête POST). Nous utilisons la méthode findOne() dans notre modèle/classe "Sauce" (QUI EST UN FICHIER "Sauce" DANS LA BDD !) pour trouver la sauce unique ayant le même _id que le paramètre de la requête ; La méthode findOne() renvoie une promesse. Elle prend comme argument l'objet de comparaison donc on récupère l'ID des paramètres de route.
 exports.getOneSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id }) // on remarque ici l'utilisation de la méthode params de l'objet requête
         .then(thing => res.status(200).json(thing)) // on renvoie le code de la réponse et le thing demandé
         .catch(error => res.status(404).json({ error }));
 };
 
-//LOGIQUE MéTIER POUR LIRE TOUTES LES SAUCES : je veux renvoyer la liste complète des sauces, donc j'utilise la méthode find du modèle "Sauce"  La méthode find renvoie une promesse. 
+//GET : LOGIQUE MéTIER POUR LIRE TOUTES LES SAUCES : je veux renvoyer la liste complète des sauces, donc j'utilise la méthode find du modèle "Sauce"  La méthode find renvoie une promesse. 
 exports.getAllSauces = (req, res, next) => {
     Sauce.find()
       .then(sauces => res.status(200).json(sauces)) //ici on renvoie en réponse un code et le tableau appelé "things" renvoyé par la BDD qui contient la liste de toutes les sauces
